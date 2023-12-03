14f40ad336a823ec78d80492380a234c9b636516
besia7 <145292459+besia7@users.noreply.github.com>
2023-12-03 16:09:44 +0200

    Add files via upload

---
+++ 4,3.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Базовий клас "Комп'ютер"
+public class Computer
+{
+    public string IPAddress { get; set; }
+    public int Power { get; set; }
+    public string OS { get; set; }
+
+    public Computer(string ipAddress, int power, string os)
+    {
+        IPAddress = ipAddress;
+        Power = power;
+        OS = os;
+    }
+}
+
+// Інтерфейс "Підключення"
+public interface IConnectable
+{
+    void Connect(Computer target);
+    void Disconnect(Computer target);
+    void TransmitData(Computer target, string data);
+    void ReceiveData(string data);
+}
+
+// Клас "Сервер"
+public class Server : Computer, IConnectable
+{
+    public Server(string ipAddress, int power, string os)
+        : base(ipAddress, power, os)
+    {
+    }
+
+    public void Connect(Computer target)
+    {
+        Console.WriteLine($"Server {IPAddress} is connected to {target.IPAddress}.");
+    }
+
+    public void Disconnect(Computer target)
+    {
+        Console.WriteLine($"Server {IPAddress} is disconnected from {target.IPAddress}.");
+    }
+
+    public void TransmitData(Computer target, string data)
+    {
+        Console.WriteLine($"Server {IPAddress} is transmitting data to {target.IPAddress}: {data}");
+    }
+
+    public void ReceiveData(string data)
+    {
+        Console.WriteLine($"Server {IPAddress} received data: {data}");
+    }
+}
+
+// Клас "Робоча станція"
+public class Workstation : Computer, IConnectable
+{
+    public Workstation(string ipAddress, int power, string os)
+        : base(ipAddress, power, os)
+    {
+    }
+
+    public void Connect(Computer target)
+    {
+        Console.WriteLine($"Workstation {IPAddress} is connected to {target.IPAddress}.");
+    }
+
+    public void Disconnect(Computer target)
+    {
+        Console.WriteLine($"Workstation {IPAddress} is disconnected from {target.IPAddress}.");
+    }
+
+    public void TransmitData(Computer target, string data)
+    {
+        Console.WriteLine($"Workstation {IPAddress} is transmitting data to {target.IPAddress}: {data}");
+    }
+
+    public void ReceiveData(string data)
+    {
+        Console.WriteLine($"Workstation {IPAddress} received data: {data}");
+    }
+}
+
+// Клас "Маршрутизатор"
+public class Router : Computer, IConnectable
+{
+    public Router(string ipAddress, int power, string os)
+        : base(ipAddress, power, os)
+    {
+    }
+
+    public void Connect(Computer target)
+    {
+        Console.WriteLine($"Router {IPAddress} is connected to {target.IPAddress}.");
+    }
+
+    public void Disconnect(Computer target)
+    {
+        Console.WriteLine($"Router {IPAddress} is disconnected from {target.IPAddress}.");
+    }
+
+    public void TransmitData(Computer target, string data)
+    {
+        Console.WriteLine($"Router {IPAddress} is transmitting data to {target.IPAddress}: {data}");
+    }
+
+    public void ReceiveData(string data)
+    {
+        Console.WriteLine($"Router {IPAddress} received data: {data}");
+    }
+}
+
+// Клас "Мережа"
+public class Network
+{
+    private List<IConnectable> devices;
+
+    public Network(List<IConnectable> devices)
+    {
+        this.devices = devices;
+    }
+
+    public void SimulateInteraction()
+    {
+        Random random = new Random();
+
+        foreach (var device in devices)
+        {
+            var otherDevice = devices[random.Next(devices.Count)];
+            if (device != otherDevice)
+            {
+                device.Connect(otherDevice as Computer);
+                device.TransmitData(otherDevice as Computer, "Sample data");
+                device.Disconnect(otherDevice as Computer);
+            }
+        }
+    }
+}
+
+// Приклад використання:
+
+class Program
+{
+    static void Main()
+    {
+        Server server = new Server("192.168.1.1", 1000, "Windows Server");
+        Workstation workstation = new Workstation("192.168.1.2", 500, "Windows 10");
+        Router router = new Router("192.168.1.3", 800, "RouterOS");
+
+        List<IConnectable> devices = new List<IConnectable> { server, workstation, router };
+        Network network = new Network(devices);
+        network.SimulateInteraction();
+    }
+}
---
+++ 4.1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Базовий клас "Живий організм"
+public class LivingOrganism
+{
+    public int Energy { get; set; }
+    public int Age { get; set; }
+    public int Size { get; set; }
+
+    public LivingOrganism(int energy, int age, int size)
+    {
+        Energy = energy;
+        Age = age;
+        Size = size;
+    }
+}
+
+// Інтерфейс "Можливість розмноження"
+public interface IReproducible
+{
+    void Reproduce();
+}
+
+// Інтерфейс "Можливість полювання"
+public interface IPredator
+{
+    void Hunt(LivingOrganism prey);
+}
+
+// Клас "Тварина"
+public class Animal : LivingOrganism, IReproducible, IPredator
+{
+    public int Speed { get; set; }
+
+    public Animal(int energy, int age, int size, int speed)
+        : base(energy, age, size)
+    {
+        Speed = speed;
+    }
+
+    public void Reproduce()
+    {
+        Console.WriteLine("Animal is reproducing.");
+    }
+
+    public void Hunt(LivingOrganism prey)
+    {
+        Console.WriteLine("Animal is hunting.");
+    }
+}
+
+// Клас "Рослина"
+public class Plant : LivingOrganism, IReproducible
+{
+    public int SunlightNeeded { get; set; }
+
+    public Plant(int energy, int age, int size, int sunlightNeeded)
+        : base(energy, age, size)
+    {
+        SunlightNeeded = sunlightNeeded;
+    }
+
+    public void Reproduce()
+    {
+        Console.WriteLine("Plant is producing seeds.");
+    }
+}
+
+// Клас "Мікроорганізм"
+public class Microorganism : LivingOrganism, IReproducible
+{
+    public double DivisionRate { get; set; }
+
+    public Microorganism(int energy, int age, int size, double divisionRate)
+        : base(energy, age, size)
+    {
+        DivisionRate = divisionRate;
+    }
+
+    public void Reproduce()
+    {
+        Console.WriteLine("Microorganism is dividing.");
+    }
+}
+
+// Клас "Екосистема"
+public class Ecosystem
+{
+    private List<LivingOrganism> organisms;
+
+    public Ecosystem(List<LivingOrganism> organisms)
+    {
+        this.organisms = organisms;
+    }
+
+    public void SimulateInteraction()
+    {
+        Random random = new Random();
+
+        foreach (var organism in organisms)
+        {
+            var otherOrganism = organisms[random.Next(organisms.Count)];
+            if (organism != otherOrganism)
+            {
+                if (organism is IReproducible && organism.Energy > 50)
+                {
+                    (organism as IReproducible).Reproduce();
+                }
+                else if (organism is IPredator && organism.Energy > 30)
+                {
+                    (organism as IPredator).Hunt(otherOrganism);
+                }
+                else
+                {
+                    Console.WriteLine($"{organism.GetType().Name} is resting.");
+                }
+            }
+        }
+    }
+}
+
+// Приклад використання:
+
+class Program
+{
+    static void Main()
+    {
+        Animal lion = new Animal(70, 5, 3, 20);
+        Plant tree = new Plant(50, 10, 2, 30);
+        Microorganism bacteria = new Microorganism(30, 1, 1, 0.5);
+
+        List<LivingOrganism> organisms = new List<LivingOrganism> { lion, tree, bacteria };
+        Ecosystem ecosystem = new Ecosystem(organisms);
+        ecosystem.SimulateInteraction();
+    }
+}
